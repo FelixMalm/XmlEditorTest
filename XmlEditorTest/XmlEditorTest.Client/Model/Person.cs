7269549ceb7b93e54beb2a36f0e5391f52153cd4
FelixMalm <felix_malm98@hotmail.com>
2024-09-30 21:08:52 +0200

    Import XML as classes

--- XmlEditorTest/XmlEditorTest.Client/Model/Person.cs
+++ XmlEditorTest/XmlEditorTest.Client/Model/Person.cs
@@ -1,11 +1,43 @@
 ﻿using System.Xml.Serialization;
 
-[XmlRoot("Person")]
-public class Person
-{
-    [XmlElement("Name")]
-    public string Name { get; set; }
 
-    [XmlElement("Age")]
-    public int Age { get; set; }
+// NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+[System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+public partial class Person
+{
+
+    private string nameField;
+
+    private byte ageField;
+
+    /// <remarks/>
+    public string Name
+    {
+        get
+        {
+            return this.nameField;
+        }
+        set
+        {
+            this.nameField = value;
+        }
+    }
+
+    /// <remarks/>
+    public byte Age
+    {
+        get
+        {
+            return this.ageField;
+        }
+        set
+        {
+            this.ageField = value;
+        }
+    }
 }
+
