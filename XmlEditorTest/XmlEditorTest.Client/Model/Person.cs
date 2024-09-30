061d7f81dd7b764d6a2e418b160e417b129f6e3b
FelixMalm <felix_malm98@hotmail.com>
2024-09-30 20:41:51 +0200

    Add project files.

---
+++ XmlEditorTest/XmlEditorTest.Client/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+[XmlRoot("Person")]
+public class Person
+{
+    [XmlElement("Name")]
+    public string Name { get; set; }
+
+    [XmlElement("Age")]
+    public int Age { get; set; }
+}
---
+++ XmlEditorTest/XmlEditorTest.Client/Service/XmlConverterService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+public class XmlConverterService
+{
+    public async Task<T> DeserializeXmlFileToObjectAsync<T>(Stream xmlFileStream)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+        using (xmlFileStream)
+        {
+            try
+            {
+                // Read the stream into a string asynchronously
+                using (var reader = new StreamReader(xmlFileStream))
+                {
+                    string xmlContent = await reader.ReadToEndAsync();
+
+                    // Deserialize from string
+                    using (var stringReader = new StringReader(xmlContent))
+                    {
+                        return (T)serializer.Deserialize(stringReader);
+                    }
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Log or handle the exception (ex.Message) for debugging
+                throw new ApplicationException("Deserialization failed.", ex);
+            }
+        }
+    }
+}
